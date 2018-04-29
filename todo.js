45f5693d04c20f2231315bc1d89a441ee079c13c
manoj1202 <manoj.appalaneni@gmail.com>
2018-04-29 15:46:35 -0500

    Arrays

--- todo.js
+++ todo.js
@@ -3,8 +3,8 @@
 const todo = ['Make break fast', 'Get Ready', 'Walk the Dog',
                         'Play some Game', 'Go for a Walk'];
 
-let x = todo.length;
-console.log(`You Have ${x} things to do `);
+
+console.log(`You Have ${todo.length} things to do `);
 
 for(let y = 0; y < todo.length; y++) {
     let z = todo[y]
