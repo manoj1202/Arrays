9542d1bcb485540bddc5980542087a999a931c4a
manoj1202 <manoj.appalaneni@gmail.com>
2018-04-29 15:32:11 -0500

    Arrays

---
+++ todo.js
@@ -0,0 +1,10 @@
+// challenge 
+
+const todo = ['Make break fast', 'Get Ready', 'Walk the Dog',
+                        'Play some Game', 'Go for a Walk'];
+
+console.log(`You Have ${todo.lenght} things to do `);
+
+
+
+
