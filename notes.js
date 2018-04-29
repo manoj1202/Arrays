074862df313db1317436c75999c60f0010c4efed
manoj1202 <manoj.appalaneni@gmail.com>
2018-04-29 16:29:52 -0500

    Arrays

--- notes.js
+++ notes.js
@@ -2,11 +2,13 @@
 const notes = ['Note 1', 'Note 2', 'Note 3'];
 
 // Deleting and Inserting an Array element from Last 
-// notes.push('Note 4');
-// notes.pop();
+// notes.push('Note 4'); //Inserting 
+// notes.pop();           // Deleting
 
 notes.unshift("First note");
+notes.shift();
 
 console.log(notes.length);
 console.log(notes); // retriving elements from font
+
 // console.log(notes[notes.length - 2]); // retriving elements from last 
