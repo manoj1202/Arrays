18ccf7042ef69e22f3dd7d9f87ddd16562ef13dd
manoj1202 <manoj.appalaneni@gmail.com>
2018-04-29 14:46:19 -0500

    Arrays

--- notes.js
+++ notes.js
@@ -3,4 +3,4 @@ const notes = ['Note 1', 'Note 2', 'Note 3'];
 
 console.log(notes.length);
 console.log(notes[2]); // retriving elements from font
-console.log(notes.length - 2);
+console.log(notes[notes.length - 2]);
