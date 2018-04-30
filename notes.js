27bbca4b2cbf514ed6c529831eced843a4f66723
manoj1202 <manoj.appalaneni@gmail.com>
2018-04-30 14:16:05 -0500

    Arrays with indexOf()

--- notes.js
+++ notes.js
@@ -9,12 +9,12 @@ const notes = ['Note 1', 'Note 2', 'Note 3'];
 //  notes.unshift("First note"); // Inserting
 //  notes.shift();              // Deleting 
 
-notes.splice(1,1,'New Note Two'); // starts at index " 1" and delete "1" element and inserts "new element"
-notes.splice(1, 0, 'New Note Two.1'); // starts at index " 1" and delete "nothing"(As it cannot go back) element and inserts "new element"
-notes.splice(1,2);
+// notes.splice(1,1,'New Note Two'); // starts at index " 1" and delete "1" element and inserts "new element"
+// notes.splice(1, 0, 'New Note Two.1'); // starts at index " 1" and delete "nothing"(As it cannot go back) element and inserts "new element"
+// notes.splice(1,2);
 
 
-notes[2] = 'New Note 2';
+// notes[2] = 'New Note 2';
 console.log(notes.length);
 console.log(notes); // retriving elements from font
 
@@ -23,3 +23,4 @@ console.log(notes); // retriving elements from font
 console.log('-----------------------------------------------');
 
 console.log(notes.indexOf('Note 2'));
+console.log(notes.indexOf('Note 3'))
