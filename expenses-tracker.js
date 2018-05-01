646bee7da1231705dd096e57591921f2ae12ea61
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-01 11:30:08 -0500

    Bulis using Arrays

--- expenses-tracker.js
+++ expenses-tracker.js
@@ -13,12 +13,20 @@ const account = {
             totalExpenses = totalExpenses + expense.cost;
         })
         return `${this.name} has $${totalExpenses} in expenses.`
+    },
+    income : [],
+    addIncome : function(description, amount){
+        this.income.push({
+          description : description,
+          amount : amount ,
+        })
     }
 }
 
 
 
-account.addExpenses('Rent', 150);
+account.addExpenses('Rent', 550);
 account.addExpenses('Coffee', 2);
+account.addIncome('job', 1000);
 console.log(account);
 console.log(account.getAccountSummary());
