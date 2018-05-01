8b15ae415ed00e680df21338b6649dddfcc63723
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-01 11:22:33 -0500

    Bulis using Arrays

--- expenses-tracker.js
+++ expenses-tracker.js
@@ -9,6 +9,9 @@ const account = {
     },
     getAccountSummary : function () {
         let totalExpenses = 0;
+        this.expenses.forEach(function(expense){
+            totalExpenses = totalExpenses + expense.cost;
+        })
         return `${this.name} has $${totalExpenses} in expenses.`
     }
 }
